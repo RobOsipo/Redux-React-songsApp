9e4a68d8669cc7dce06ad55d7000b5382f3a46bf
Robert <osipovitchr@gmail.com>
2022-03-31 14:24:12 -0400

    react 18 update to code + mapStateToProps redux

--- redux-songs/src/index.js
+++ redux-songs/src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+// import createRoot from 'react-dom/client'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
 import App from './components/App.js'
+import reducers from './reducers/index.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(
+//     <Provider store={createStore(reducers)}>
+//          <App />
+//     </Provider>,
+//  document.getElementById('root'));
 
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
+ <Provider store={createStore(reducers)}>
+ <App />
+</Provider>
+)
